5ed665b346562806e2aba7dee1efd3fafbf46c22
Alysson93 <alyssonpereiraassuncao@gmail.com>
2025-04-06 16:26:08 -0300

    primeira versão completa

--- js/app.js
+++ js/app.js
@@ -1,24 +1,64 @@
+let listaNumerosSorteados = [];
+let tentativas = 1;
+
 function exibeTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 function getRandomNumber() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    if (listaNumerosSorteados.includes(numeroEscolhido) && listaNumerosSorteados.length < 10) {
+        return getRandomNumber();
+    }
+    listaNumerosSorteados.push(numeroEscolhido);
+    return numeroEscolhido;
+}
+
+function limparCampo(tag) {
+    chute = document.querySelector(tag).value = '';
 }
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute > secretNumber) {
         exibeTexto('p', 'O número secreto é menor.');
+        limparCampo('input');
     } else if (chute < secretNumber) {
         exibeTexto('p', 'O número secreto é maior.');
+        limparCampo('input');
     } else {
         exibeTexto('h1', 'Acertou!');
+        let plural = tentativas > 1 ? 'tentativas' : 'tentativa';
+        exibeTexto('p', `Você descobriu o número secreto com ${tentativas} ${plural}.`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('chutar').setAttribute('disabled', true);
     }
+    tentativas++;
 }
 
-exibeTexto('h1', 'Jogo do número secreto');
-exibeTexto('p', 'Escolha um número entre 1 e 10');
+function reiniciarJogo() {
+    secretNumber = getRandomNumber();
+    limparCampo('input');
+    tentativas = 1;
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    document.getElementById('chutar').removeAttribute('disabled');
+}
+
+function mensagemInicial() {
+    exibeTexto('h1', 'Jogo do número secreto');
+    exibeTexto('p', 'Escolha um número entre 1 e 10');
+}
 
 let secretNumber = getRandomNumber();
+
+mensagemInicial();
